b6e2d7985f722ac623b3a5b62c1c91b721b3d022
casualme7 <miljan.pantovic@gmail.com>
2022-03-26 16:36:00 +0100

    Small update

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ let nextB = document.querySelector("#nextB");
 let laughB = document.querySelector("#laughB");
 let header = document.querySelector(".header");
 let popUp = document.querySelector("#popUp");
+let sideMenu = document.querySelector("#sideMenu");
+let sideMenuButtonExit = document.querySelector("#sideMenuButtonExit");
+let sideMenuButtonOpen = document.querySelector("#sideMenuButtonOpen");
+let memesSeenSpan = document.querySelector("#memesSeenSpan");
+let memesLikedSpan = document.querySelector("#memesLikedSpan");
 let fullScreen = document.querySelector("#fullScreen");
 let API_LINK = "https://meme-api.herokuapp.com/gimme";
 
@@ -47,17 +52,35 @@ let memeGenerator = async () => {
 	memeImage.src = response.data.url;
 };
 
+// Local Storage for MEMES and LIKES information
+let memesSeen = null;
+let memesLocalScorage = localStorage.MEMES_SEEN;
+if (memesLocalScorage) {
+	memesSeen = memesLocalScorage;
+} else {
+	memesSeen = 0;
+}
+memesSeenSpan.innerText = memesSeen;
+//
+let memesLiked = null;
+let memesLikedStorage = localStorage.MEMES_LIKED;
+if (memesLikedStorage) {
+	memesLiked = memesLikedStorage;
+} else {
+	memesLiked = 0;
+}
+memesLikedSpan.innerText = memesLiked;
+
 // Next BUTTON that generates next IMG
 let browsingStarted = false;
 nextB.addEventListener("click", async () => {
+	memesSeen++;
+	localStorage.setItem("MEMES_SEEN", memesSeen);
+	memesSeenSpan.innerText = memesSeen;
 	clickSound.play();
 	memeGenerator();
 	browsingStarted = true;
 	// Added just to stop animated header on Next Button
-	header.innerHTML = "";
-	for (let i = 0; i < 99; i++) {
-		clearInterval(i);
-	}
 });
 
 // Laugh BUTTON that SAVES the img *WORK IN PROGRESS*
@@ -65,6 +88,9 @@ let laughBx = true;
 laughB.addEventListener("click", () => {
 	clickSound.play();
 	if (laughBx && browsingStarted) {
+		memesLiked++;
+		localStorage.setItem("MEMES_LIKED", memesLiked);
+		memesLikedSpan.innerText = memesLiked;
 		laughBx = false;
 		browsingStarted = false;
 		laughEffect.play();
@@ -95,6 +121,16 @@ function openFullscreen() {
 		elem.msRequestFullscreen();
 	}
 }
+
+// EXIT
+sideMenuButtonExit.addEventListener("click", () => {
+	sideMenu.style.right = "-250px";
+});
+
+// OPEN
+sideMenuButtonOpen.addEventListener("click", () => {
+	sideMenu.style.right = "0px";
+});
 
 // Miscellaneous
 let lastUpdatedTime = new Date("3/25/2022, 2:14:42 PM");
